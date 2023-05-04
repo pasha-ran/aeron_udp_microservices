bd20eb8796d35cf8fa80b898aa5ca4ec32d9daf7
Naseefabu <naseefbloomberg@gmail.com>
2023-05-04 10:38:45 +0530

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include "Aeron.h"
+using namespace aeron;
 
 int main(){
+
+    Context context;
+    Aeron aeron(context);
+
     std::cout << "Aeron UDP microservices " << std::endl;
 }
